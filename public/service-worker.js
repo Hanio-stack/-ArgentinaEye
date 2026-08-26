15100f56ed4848ae1aba463582f00132068d6605
Hanio-stack <294352592+Hanio-stack@users.noreply.github.com>
2026-08-26 13:43:25 +0900

    chore: refresh PWA cache for public description

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="argentina-eye-v4";
+const CACHE="argentina-eye-v5";
 const DATA_KEY="data/latest.json";
 const CORE=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg",DATA_KEY];
 
